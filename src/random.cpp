964e7fee958a23d14b3562c13735a1ce1ac25239
chenyunyun <chenyunyun@bj48-28.i.netease.com>
2014-02-27 21:26:15 +0800

    implement random hash function

--- src/random.cpp
+++ src/random.cpp
@@ -1,1 +1,14 @@
 #include "random.h"
+
+Random::Random(uint32_t seed): seed_(seed&0x7fffffff){
+}
+
+//模仿RS hash, 作为内部实现
+uint32_t Random::next(){
+    static uint32_t b = 378551;
+    static uint32_t a = 63689;
+
+    seed_ = (uint32_t)(seed_*a + b);
+
+    return seed_;
+}
--- src/random.h
+++ src/random.h
@@ -2,10 +2,12 @@
 #define __RANDOM_H__
 
 #include <stdint.h>
+#include <assert.h>
 
 //32位无符号随机数发生器
-class Radom{
+class Random{
 public:
+    //seed 一般使用time(NULL)提供即可
     Random(uint32_t seed);
 
 public:
@@ -13,13 +15,20 @@ public:
     uint32_t next();
 
     //对随机数进行格式化: 范围[0, max-1]
-    uint32_t uniform(uint32_t max);
+    uint32_t uniform(uint32_t max){
+        return next()%max;
+    }
 
     // 1/n 概率
-    bool one_in(uint32_t n);
+    bool one_in(uint32_t n){
+        return next()%n == 0;
+    }
 
     // n/m 概率
-    bool range_in();
+    bool range_in(uint32_t m, uint32_t n){
+        assert(m >= n);
+        return next()%m < n;
+    }
 
 private:
     uint32_t seed_;
